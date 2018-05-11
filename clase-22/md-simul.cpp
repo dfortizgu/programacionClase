cc7686477946a8e85822186618677842b9aa47ef
dfortizgu <dfortizgu@unal.edu.co>
2018-05-11 09:01:01 -0500

    clase

---
+++ clase-22/md-simul.cpp
@@ -0,0 +1,66 @@
+#include "md-simul.h"
+
+void initial_conditions(Particle & body)
+{
+  body.Ry = 1.6598;
+  body.Vx = 0.23698;
+
+  body.rad = 0.235;
+  body.mass = 0.29698;
+}
+
+void compute_force(Particle & body)
+{
+  // reset force
+  body.Fx = body.Fy = body.Fz = 0.0;
+
+  // gravitational force
+  body.Fy += body.mass*G;
+
+  // force with ground
+  double deltag = body.rad - body.Ry;
+  if (deltag > 0) {
+    body.Fy += K*deltag;
+    //body.Fy -= 1.9876*body.Vy; 
+  }
+  //Wall
+  double deltaWx =body.rad -(body.Rx-0.5);
+  if(deltaWx < 0){
+    body.Fx +=K*deltaWx;
+  }
+
+  double deltaWy =body.rad -(body.Ry-0.5);
+  if(deltaWy < 0){
+    body.Fx +=K*deltaWy;
+  }
+}
+
+void start_integration(Particle & body, const double & dt)
+{
+  body.Vx -= body.Fx*dt/(2*body.mass);
+  body.Vy -= body.Fy*dt/(2*body.mass);
+  body.Vz -= body.Fz*dt/(2*body.mass);
+}
+
+  void time_integration(Particle & body, const double & dt)
+{
+  // leap-frog
+  body.Vx += body.Fx*dt/(body.mass);
+  body.Vy += body.Fy*dt/(body.mass);
+  body.Vz += body.Fz*dt/(body.mass);
+  body.Rx += body.Vx*dt; 
+  body.Ry += body.Vy*dt; 
+  body.Rz += body.Vz*dt; 
+}
+
+void print(Particle & body, double time)
+{
+  std::cout << time << "  " 
+            << body.Rx << "  "
+            << body.Ry << "  "
+            << body.Rz << "  "
+            << body.Vx << "  "
+            << body.Vy << "  "
+            << body.Vz << "  "
+            << "\n";
+}
